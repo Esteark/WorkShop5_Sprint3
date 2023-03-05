9c37eb944941b11598f2f041993b944acd0aea53
Pangie <angierincon326@gmail.com>
2023-03-05 17:19:50 -0500

    form pago

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/home/Home";
 import HomeLogin from "../components/login/HomeLogin";
+import SeccionPago from "../components/seccionPago/SeccionPago";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeLogin />} />
+        <Route path="/" element={<SeccionPago />} />
       </Routes>
     </BrowserRouter>
   );
