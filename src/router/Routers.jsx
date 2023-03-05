1768957ba54ecd6e6f0fa99e7a004f7e930e0de8
Pangie <angierincon326@gmail.com>
2023-03-05 16:24:27 -0500

    login listo

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/home/Home";
+import HomeLogin from "../components/login/HomeLogin";
+import SeccionPago from "../components/seccionPago/SeccionPago";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<SeccionPago />} />
       </Routes>
     </BrowserRouter>
   );
