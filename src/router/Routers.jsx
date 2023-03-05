6fb362a2b6b74883316944b75ca7972f28eab372
Pangie <angierincon326@gmail.com>
2023-03-05 17:09:13 -0500

    Solo login

--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -2,13 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../components/home/Home";
 import HomeLogin from "../components/login/HomeLogin";
-import SeccionPago from "../components/seccionPago/SeccionPago";
 
 const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SeccionPago />} />
+        <Route path="/" element={<HomeLogin />} />
       </Routes>
     </BrowserRouter>
   );
